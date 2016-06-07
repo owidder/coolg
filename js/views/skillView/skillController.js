11cfb1135f543020d5455361226069903ca0d88c
Oliver Widder <oliver.widder@gmail.com>
2016-06-07 19:22:08 +0200

    Tue Jun  7 19:22:08 CEST 2016

--- js/views/skillView/skillController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-com_geekAndPoke_coolg.SKILL_CONTROLLER = "skillController";
-
-angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_coolg.SKILL_CONTROLLER, function($scope) {
-
-});
