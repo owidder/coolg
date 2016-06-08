13774f3b51406429c477fe11e0fc2ca948b5ce01
Oliver Widder <oliver.widder@gmail.com>
2016-06-08 08:31:06 +0200

    Wed Jun  8 08:31:06 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -3,7 +3,7 @@
 com_geekAndPoke_coolg.SKILL_CONTROLLER = "skillController";
 
 angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_coolg.SKILL_CONTROLLER,
-    function ($scope, $routeParams, $location, $timeout, $q, dimensions) {
+    function ($scope, $routeParams, $location, $timeout, $q, dimensions, funcs) {
         var Skills = bottle.container.Skills;
         var dl = bottle.container.$datalib;
 
