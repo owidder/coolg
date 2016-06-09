cff9376395edbd3484be7c166e84046d814d9e69
Oliver Widder <oliver.widder@gmail.com>
2016-06-09 08:39:23 +0200

    Thu Jun  9 08:39:23 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -28,8 +28,8 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
             });
         }
 
-        var width = (dimensions.screenDimensions.width - 50);
-        var height = (dimensions.screenDimensions.height - 70);
+        var width = (dimensions.screenDimensions.width - 50) * 2;
+        var height = (dimensions.screenDimensions.height - 70) * 2;
         var margin = {"left": 100, "bottom": 25, "right": 5};
 
         var svg = d3.select("#canvas")
