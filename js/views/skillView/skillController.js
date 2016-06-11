dfd912c8ef1fbfd6fcc847c0f73df838727151dc
Oliver Widder <oliver.widder@gmail.com>
2016-06-11 16:50:33 +0200

    Sat Jun 11 16:50:33 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -115,9 +115,9 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
 
             horizontalLine.transition()
                 .attr("x1", 0)
-                .attr("y1", yScale((maxY-1)/2))
+                .attr("y1", yScale(maxY/2 + 1))
                 .attr("x2", xScale(maxX))
-                .attr("y2", yScale((maxY-1)/2));
+                .attr("y2", yScale(maxY/2 + 1));
 
             verticalLine.transition()
                 .attr("x1", xScale((maxX-1)/2))
