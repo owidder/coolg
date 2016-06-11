6c0258176dfa3473d21595d842abca2d8acb6c2d
Oliver Widder <oliver.widder@gmail.com>
2016-06-11 09:29:40 +0200

    Sat Jun 11 09:29:40 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -295,10 +295,6 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
             $scope.hideButtons = $routeParams.hb;
 
             $scope.allCategories = skills.categories;
-            $scope.categorySwitchModel = {'*': true};
-            $scope.allCategories.forEach(function(category) {
-                $scope.categorySwitchModel[category] = false;
-            });
 
             drawSkills();
 
