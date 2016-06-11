31d55dd00082e3ef619c0ca119998fcfbe247677
Oliver Widder <oliver.widder@gmail.com>
2016-06-11 17:02:46 +0200

    Sat Jun 11 17:02:46 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -44,11 +44,11 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
 
         root.append("g")
             .attr("class", "axis xaxis")
-            .attr("transform", "translate(0," + (height+50) + ")");
+            .attr("transform", "translate(0," + (height+25) + ")");
 
         root.append("g")
             .attr("class", "axis yaxis")
-            .attr("transform", "translate(0, 50)");
+            .attr("transform", "translate(-25, 50)");
 
         var xLabel = root.append("text")
             .attr("x", 0)
