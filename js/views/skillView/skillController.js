05804d94c0e59b36f49a4571ada4b15f8b20d1e4
Oliver Widder <oliver.widder@gmail.com>
2016-06-12 12:54:48 +0200

    Sun Jun 12 12:54:48 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -61,8 +61,8 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
         function moveLegend(x, y) {
             var elements = getIntersectList(x, y);
             var skillStrList = createIntersectSkillList(elements);
-            console.log(elements);
-            
+            console.log(skillStrList);
+
             legend
                 .attr("transform", "translate(" + x + "," + y + ")");
         }
