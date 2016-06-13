5badcf95cc92d1f39d4ce808adaffddfb51996b4
Oliver Widder <oliver.widder@gmail.com>
2016-06-13 08:31:02 +0200

    Mon Jun 13 08:31:02 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -41,7 +41,7 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
                         meanAssess = mathUtil.round(svgElement.getAttribute("_ma"), 1);
                         meanDuration = mathUtil.round(svgElement.getAttribute("_md"), 1);
                         category = svgElement.getAttribute("_cat");
-                        skillStr = skillName + " [" + category + "] " + count + " / " + meanAssess + " / " + meanDuration;
+                        skillStr = skillName + " [" + category + "] - " + count + " / " + meanAssess + " / " + meanDuration;
                         skillList.push(skillStr)
                     }
                 }
