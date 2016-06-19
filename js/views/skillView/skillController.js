d33c5d8892fc13bea4c9b99d50328e10ecb2f6a3
Oliver Widder <oliver.widder@gmail.com>
2016-06-19 15:28:43 +0200

    Sun Jun 19 15:28:43 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -668,6 +668,7 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
         }
 
         function textIsShown() {
+            console.log($scope.input);
             return svg.select("text.skill.off").size() == 0;
         }
 
