7146a706e33a645ae004336f9df50f22da6e82f8
Oliver Widder <oliver.widder@gmail.com>
2016-06-20 18:09:26 +0200

    Mon Jun 20 18:09:26 CEST 2016

--- demo/treemap/treemap.js
+++ demo/treemap/treemap.js
@@ -5,7 +5,9 @@ var w = 960,
 var treemap = d3.layout.treemap()
     .padding(4)
     .size([w, h])
-    .value(function(d) { return d.size; });
+    .value(function(d) {
+        return d.count;
+    });
 
 var svg = d3.select("body").append("svg:svg")
     .style("width", w)
@@ -13,7 +15,7 @@ var svg = d3.select("body").append("svg:svg")
     .append("svg:g")
     .attr("transform", "translate(-.5,-.5)");
 
-d3.json("readme.json", function(json) {
+d3.json("skills.json", function(json) {
     var cell = svg.data([json]).selectAll("g")
         .data(treemap)
         .enter().append("svg:g")
