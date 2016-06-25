1ad19bc9adb97bdba9fbe1cfe0bb59cef12f5867
Oliver Widder <oliver.widder@gmail.com>
2016-06-25 09:35:37 +0200

    Sat Jun 25 09:35:37 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -292,7 +292,7 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
                 .attr("class", "y axis wcm-label")
                 .attr("transform", "rotate(270 " + yLabelX + "," + yLabelY + ")");
 
-            field = root.append("g")
+            var field = root.append("g")
                 .attr("class", "field")
                 .attr("transform", "translate(0, 0)");
 
