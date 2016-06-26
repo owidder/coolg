1c83a4f8551ff33faf3780b7c03fd1784f29e3cd
Oliver Widder <oliver.widder@gmail.com>
2016-06-26 09:01:10 +0200

    Sun Jun 26 09:01:10 CEST 2016

--- js/views/skillView/Skills.js
+++ js/views/skillView/Skills.js
@@ -126,7 +126,7 @@ bottle.factory("Skills", function(container) {
             var filteredData = rawData.filter(filter);
             var aggregation = dl.groupby('Skill')
                 .summarize([
-                    {name: constants.COLUMN_BEWERTUNG, ops: ['mean'], as: ['Mittlere Bewertung']},
+                    {name: constants.COLUMN_BEWERTUNG, ops: ['mean'], as: [constants.ATTRIBUTE_MITTLERE_BEWERTUNG]},
                     {name: constants.COLUMN_ANZAHL_MITARBEITER, ops: ['sum'], as: ['Anzahl Mitarbeiter']},
                     {
                         name: 'Anzahl Experten',
