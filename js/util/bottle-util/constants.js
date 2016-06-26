b48b845883fe05fcdcbb683ff6f61a23e994663a
Oliver Widder <oliver.widder@gmail.com>
2016-06-26 08:31:14 +0200

    Sun Jun 26 08:31:14 CEST 2016

--- js/util/bottle-util/constants.js
+++ js/util/bottle-util/constants.js
@@ -6,6 +6,7 @@ bottle.factory("constants", function(container) {
         START_DATE: "1980-01-01",
         END_DATE: "2015-03-01",
         STOCK_PROPERTY_NAME: "Close",
+        ATTRIBUTE_SKILL: "Skill",
         ATTRIBUTE_ANZAHL_EXPERTEN: "expertCount",
         ATTRIBUTE_ANZAHL_MITARBEITER: "count",
         ATTRIBUTE_MITTLERE_BEWERTUNG: "ma",
