a999e6250dd4ede001699af631a009324d017d27
Oliver Widder <oliver.widder@gmail.com>
2016-06-26 08:26:32 +0200

    Sun Jun 26 08:26:32 CEST 2016

--- js/util/bottle-util/constants.js
+++ js/util/bottle-util/constants.js
@@ -5,6 +5,15 @@ bottle.factory("constants", function(container) {
     return {
         START_DATE: "1980-01-01",
         END_DATE: "2015-03-01",
-        STOCK_PROPERTY_NAME: "Close"
+        STOCK_PROPERTY_NAME: "Close",
+        ATTRIBUTE_ANZAHL_EXPERTEN: "expertCount",
+        ATTRIBUTE_ANZAHL_MITARBEITER: "count",
+        ATTRIBUTE_MITTLERE_BEWERTUNG: "ma"
     }
 });
+
+var ATTRIBUTE_ANZAHL_EXPERTEN = "expertCount";
+var ATTRIBUTE_ANZAHL_MITARBEITER = "count";
+var ATTRIBUTE_MITTLERE_BEWERTUNG = "ma";
+var ATTRIBUTE_MITTLERE_DAUER = "md";
+
