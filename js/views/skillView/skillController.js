a04557cd3f86041086b3fbab57af1cc184ee699f
Oliver Widder <oliver.widder@gmail.com>
2016-06-26 08:01:35 +0200

    Sun Jun 26 08:01:35 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -876,6 +876,7 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
             root.selectAll("g.cell").select("text.skill");
 
             root.selectAll("text.skill")
+                .transition().duration(1000)
                 .attr("x", function(d) {
                     return d.dx / 2;
                 })
