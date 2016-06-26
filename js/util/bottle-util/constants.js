770ffc7f00f6828808c8236269ca486b4066f5e1
Oliver Widder <oliver.widder@gmail.com>
2016-06-26 08:33:25 +0200

    Sun Jun 26 08:33:25 CEST 2016

--- js/util/bottle-util/constants.js
+++ js/util/bottle-util/constants.js
@@ -12,6 +12,7 @@ bottle.factory("constants", function(container) {
         ATTRIBUTE_MITTLERE_BEWERTUNG: "ma",
         ATTRIBUTE_MITTLERE_DAUER: "md",
         COLUMN_SKILL_UNTERKATEGORIE: "Skill-Unterkategorie",
-        COLUMN_STANDORT: "Standort"
+        COLUMN_STANDORT: "Standort",
+        COLUMN_BEWERTUNG: 
     }
 });
