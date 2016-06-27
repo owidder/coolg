e2929b03a0a86a1ef92a593407f13f441a945001
Oliver Widder <oliver.widder@gmail.com>
2016-06-27 18:21:09 +0200

    Mon Jun 27 18:21:09 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -80,6 +80,7 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
                 styleString += styleForChecked;
                 styleString += styleForUnchecked;
             });
+            styleString += "</style>\n";
         }
 
         function getSvgBoundingRectOfElement(selector) {
