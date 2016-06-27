574bf640257b4e43032c44881293b4a5b80e4d26
Oliver Widder <oliver.widder@gmail.com>
2016-06-27 08:21:41 +0200

    Mon Jun 27 08:21:41 CEST 2016

--- js/views/skillView/skillController.js
+++ js/views/skillView/skillController.js
@@ -1059,7 +1059,7 @@ angular.module(com_geekAndPoke_coolg.moduleName).controller(com_geekAndPoke_cool
             $scope.switchMode = switchMode;
             $scope.switchYAttribute = switchYAttribute;
             $scope.disableYaSwitch = disableYaSwitch;
-            $scope.showModeSwitch = showModeSwitch;
+            $scope.disableModeSwitch = disableModeSwitch;
             $scope.draw = draw;
             $scope.disableTextSwitch = disableTextSwitch;
             $scope.disableCategorySwitch = disableCategorySwitch;
